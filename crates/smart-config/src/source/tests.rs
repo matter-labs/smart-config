85173cb2cd003f354c57cb81ded07d22ea67944d
Alex Ostrovski <aov@matterlabs.dev>
2024-11-20 16:11:32 +0200

    Use `config!` macro in tests

--- crates/smart-config/src/source/tests.rs
+++ crates/smart-config/src/source/tests.rs
@@ -46,12 +46,12 @@ fn parsing() {
     let env = Environment::from_iter(
         "",
         [
-            ("int".to_owned(), "1".to_owned()),
-            ("bool".to_owned(), "true".to_owned()),
-            ("string".to_owned(), "??".to_owned()),
-            ("array".to_owned(), "1,2,3".to_owned()),
-            ("renamed".to_owned(), "first".to_owned()),
-            ("repeated".to_owned(), "second,first".to_owned()),
+            ("int", "1"),
+            ("bool", "true"),
+            ("string", "??"),
+            ("array", "1,2,3"),
+            ("renamed", "first"),
+            ("repeated", "second,first"),
         ],
     );
     let env = wrap_into_value(env);
@@ -118,6 +118,21 @@ fn parsing_enum_config() {
 fn parsing_enum_config_with_schema() {
     let schema = ConfigSchema::default().insert::<EnumConfig>("");
 
+    let json = config!(
+        "type": "Fields",
+        "string": "???",
+        "flag": true,
+    );
+    let repo = ConfigRepository::new(&schema).with(json);
+    let config: EnumConfig = repo.parse().unwrap();
+    assert_eq!(
+        config,
+        EnumConfig::WithFields {
+            string: Some("???".to_owned()),
+            flag: true,
+        }
+    );
+
     let env = Environment::from_iter(
         "",
         [
@@ -145,13 +160,7 @@ fn parsing_enum_config_with_schema() {
 
 #[test]
 fn parsing_errors() {
-    let env = Environment::from_iter(
-        "",
-        [
-            ("renamed".to_owned(), "first".to_owned()),
-            ("other_int".to_owned(), "what".to_owned()),
-        ],
-    );
+    let env = Environment::from_iter("", [("renamed", "first"), ("other_int", "what")]);
     let err = NestedConfig::deserialize(ValueDeserializer::new(&wrap_into_value(env))).unwrap_err();
 
     assert!(err.inner.to_string().contains("u32 value 'what'"), "{err}");
@@ -206,13 +215,41 @@ fn nesting_json() {
 
 #[test]
 fn merging_config_parts() {
-    let env = Environment::from_iter(
-        "",
-        [
-            ("deprecated_value".to_owned(), "4".to_owned()),
-            ("nested_renamed".to_owned(), "first".to_owned()),
-        ],
+    let json = config!(
+        "deprecated.value": 4,
+        "nested.renamed": "first",
     );
+
+    let alias = Alias::prefix("deprecated").exclude(|name| name == "not_merged");
+    let mut schema = ConfigSchema::default().insert_aliased::<ConfigWithNesting>("", [alias]);
+    schema
+        .single_mut::<NestedConfig>()
+        .unwrap()
+        .push_alias(Alias::prefix("deprecated"));
+
+    let config: ConfigWithNesting = ConfigRepository::new(&schema).with(json).parse().unwrap();
+    assert_eq!(config.value, 4);
+    assert_eq!(config.nested.simple_enum, SimpleEnum::First);
+    assert_eq!(config.nested.other_int, 42);
+
+    let json = config!(
+        "value": 123,
+        "deprecated.value": 4,
+        "nested.renamed": "first",
+        "deprecated.other_int": 321,
+        "deprecated.not_merged": "!",
+    );
+
+    let config: ConfigWithNesting = ConfigRepository::new(&schema).with(json).parse().unwrap();
+    assert_eq!(config.value, 123);
+    assert_eq!(config.not_merged, "");
+    assert_eq!(config.nested.simple_enum, SimpleEnum::First);
+    assert_eq!(config.nested.other_int, 321);
+}
+
+#[test]
+fn merging_config_parts_with_env() {
+    let env = Environment::from_iter("", [("deprecated_value", "4"), ("nested_renamed", "first")]);
 
     let alias = Alias::prefix("deprecated").exclude(|name| name == "not_merged");
     let mut schema = ConfigSchema::default().insert_aliased::<ConfigWithNesting>("", [alias]);
@@ -229,11 +266,11 @@ fn merging_config_parts() {
     let env = Environment::from_iter(
         "",
         [
-            ("value".to_owned(), "123".to_owned()),
-            ("deprecated_value".to_owned(), "4".to_owned()),
-            ("nested_renamed".to_owned(), "first".to_owned()),
-            ("deprecated_other_int".to_owned(), "321".to_owned()),
-            ("deprecated_not_merged".to_owned(), "!".to_owned()),
+            ("value", "123"),
+            ("deprecated_value", "4"),
+            ("nested_renamed", "first"),
+            ("deprecated_other_int", "321"),
+            ("deprecated_not_merged", "!"),
         ],
     );
 
@@ -324,20 +361,12 @@ fn using_aliases_with_object_config() {
     let alias = Alias::prefix("deprecated");
     let schema = ConfigSchema::default().insert_aliased::<ConfigWithNesting>("test", [alias]);
 
-    let json = serde_json::json!({
-        "value": 123, // should not be parsed
-        "deprecated": {
-            "value": 321,
-        },
-        "test": {
-            "nested": { "renamed": "first" },
-        }
-    });
-    let serde_json::Value::Object(json) = json else {
-        unreachable!();
-    };
-    let base = Json::new("base.json", json);
-    let repo = ConfigRepository::new(&schema).with(base);
+    let json = config!(
+        "value": 123, // Should not be used.
+        "deprecated.value": 321,
+        "test.nested.renamed": "first",
+    );
+    let repo = ConfigRepository::new(&schema).with(json);
 
     let config: ConfigWithNesting = repo.parse().unwrap();
     assert_eq!(config.value, 321);
@@ -350,17 +379,11 @@ fn using_env_config_overrides() {
     let alias = Alias::prefix("deprecated");
     let schema = ConfigSchema::default().insert_aliased::<ConfigWithNesting>("test", [alias]);
 
-    let json = serde_json::json!({
-        "value": 123, // should not be parsed
-        "test": {
-            "value": 321,
-            "nested": { "renamed": "first" },
-        }
-    });
-    let serde_json::Value::Object(json) = json else {
-        unreachable!();
-    };
-    let base = Json::new("base.json", json);
+    let base = config!(
+        "value": 123, // Should not be used.
+        "deprecated.value": 321,
+        "test.nested.renamed": "first",
+    );
     let mut repo = ConfigRepository::new(&schema).with(base);
 
     let config: ConfigWithNesting = repo.parse().unwrap();
