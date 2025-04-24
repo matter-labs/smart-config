f1ae722577e0a0d32c79a32483b09af7cacb040a
Alex Ostrovski <aov@matterlabs.dev>
2025-04-24 13:54:58 +0300

    Document validations

--- crates/smart-config/src/lib.rs
+++ crates/smart-config/src/lib.rs
@@ -143,8 +143,16 @@ extern crate core;
 ///
 /// ## `validate`
 ///
-/// **Type:** human-readable description + path to a function / method with the `fn(&Self) -> Result<(), ErrorWithOrigin>` signature
-/// (see [the example below](#defining-validations)).
+/// **Type:** One of the following:
+///
+/// - Expression evaluating to a [`Validate`](validation::Validate) implementation (e.g., a [`Range`](std::ops::Range); see the `Validate` docs
+///   for implementations). An optional human-readable string validation description may be provided delimited by the comma (e.g., to make the description
+///   more domain-specific).
+/// - Pointer to a function with the `fn(&_) -> Result<(), ErrorWithOrigin>` signature and the validation description separated by a comma.
+/// - Pointer to a function with the `fn(&_) -> bool` signature and the validation description separated by a comma. Validation fails
+///   if the function returns `false`.
+///
+/// See the examples in the [`validation`] module.
 ///
 /// Specifies a post-deserialization validation for the config. This is useful to check invariants involving multiple params.
 /// Multiple validations are supported by specifying the attribute multiple times.
@@ -241,6 +249,10 @@ extern crate core;
 /// If specified, the field is treated as a *flattened* sub-config rather than a param. Unlike `nest`, its params
 /// will be added to the containing config instead of a separate object. The sub-config type must implement `DescribeConfig`.
 ///
+/// ## `validate`
+///
+/// Has same semantics as [config validations](#validate), but applies to a specific config parameter.
+///
 /// # Validations
 ///
 /// The following validations are performed by the macro in compile time:
@@ -298,41 +310,6 @@ extern crate core;
 ///
 /// impl FlattenedConfig {
 ///     const fn default_array() -> [f32; 2] { [1.0, 2.0] }
-/// }
-/// ```
-///
-/// ## Defining validations
-///
-/// ```
-/// use secrecy::{ExposeSecret, SecretString};
-/// use smart_config::validation;
-/// # use smart_config::{testing, DescribeConfig, DeserializeConfig, ErrorWithOrigin};
-///
-/// #[derive(DescribeConfig, DeserializeConfig)]
-/// #[config(validate = validation::Custom(
-///     "secret key must have expected length",
-///     Self::validate_secret_key
-/// ))]
-/// struct ValidatedConfig {
-///     secret_key: SecretString,
-///     /// Reference key length. If specified, the secret key length
-///     /// will be checked against it.
-///     secret_key_len: Option<usize>,
-/// }
-///
-/// impl ValidatedConfig {
-///     fn validate_secret_key(&self) -> Result<(), ErrorWithOrigin> {
-///         if let Some(expected_len) = self.secret_key_len {
-///             let actual_len = self.secret_key.expose_secret().len();
-///             if expected_len != actual_len {
-///                 return Err(ErrorWithOrigin::custom(format!(
-///                     "unexpected `secret_key` length ({actual_len}); \
-///                      expected {expected_len}"
-///                 )));
-///             }
-///         }
-///         Ok(())
-///     }
 /// }
 /// ```
 pub use smart_config_derive::DescribeConfig;
--- crates/smart-config/src/validation/mod.rs
+++ crates/smart-config/src/validation/mod.rs
@@ -1,6 +1,55 @@
 //! Parameter and config validation.
-
-#![allow(missing_docs)] // FIXME
+//!
+//! # Overview
+//!
+//! The core validation functionality is encapsulated in the [`Validate`] trait.
+//!
+//! # Examples
+//!
+//! ```
+//! use secrecy::{ExposeSecret, SecretString};
+//! use smart_config::validation;
+//! # use smart_config::{testing, DescribeConfig, DeserializeConfig, ErrorWithOrigin};
+//!
+//! #[derive(DescribeConfig, DeserializeConfig)]
+//! #[config(validate(
+//!     Self::validate_secret_key,
+//!     "secret key must have expected length"
+//! ))]
+//! struct ValidatedConfig {
+//!     secret_key: SecretString,
+//!     /// Reference key length. If specified, the secret key length
+//!     /// will be checked against it.
+//!     #[config(validate(..=100))]
+//!     // ^ Validates that the value is in the range. Note that validations
+//!     // handle `Option`s intelligently; if the value isn't specified
+//!     // (i.e., is `None`), it will pass validation.
+//!     secret_key_len: Option<usize>,
+//!     #[config(validate(not_empty, "must not be empty"))]
+//!     app_name: String,
+//! }
+//!
+//! // We have to use `&String` rather than more idiomatic `&str` in order to
+//! // exactly match the validated type.
+//! fn not_empty(s: &String) -> bool {
+//!     !s.is_empty()
+//! }
+//!
+//! impl ValidatedConfig {
+//!     fn validate_secret_key(&self) -> Result<(), ErrorWithOrigin> {
+//!         if let Some(expected_len) = self.secret_key_len {
+//!             let actual_len = self.secret_key.expose_secret().len();
+//!             if expected_len != actual_len {
+//!                 return Err(ErrorWithOrigin::custom(format!(
+//!                     "unexpected `secret_key` length ({actual_len}); \
+//!                      expected {expected_len}"
+//!                 )));
+//!             }
+//!         }
+//!         Ok(())
+//!     }
+//! }
+//! ```
 
 use std::{fmt, ops, sync::Arc};
 
@@ -11,13 +60,23 @@ use crate::ErrorWithOrigin;
 #[doc(hidden)] // only used in proc macros
 pub mod _private;
 
-/// Generic post-validation for a configuration parameter.
+/// Generic post-validation for a configuration parameter or a config.
+///
+/// # Implementations
+///
+/// Validations are implemented for the following types:
+///
+/// - [`Range`](ops::Range), [`RangeInclusive`](ops::RangeInclusive) etc. Validate whether the type is within the provided bounds.
 pub trait Validate<T: ?Sized>: 'static + Send + Sync {
+    /// Describes this validation.
+    ///
     /// # Errors
     ///
     /// Should propagate formatting errors.
     fn describe(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result;
 
+    /// Validates a parameter / config.
+    ///
     /// # Errors
     ///
     /// Should return an error if validation fails.
