3aa67876e96bc6403e71747163c037d23d908e68
Alex Ostrovski <aov@matterlabs.dev>
2025-06-17 13:06:52 +0300

    Add snapshot tests for YAML / JSON output

--- crates/smart-config-commands/tests/integration/main.rs
+++ crates/smart-config-commands/tests/integration/main.rs
@@ -1,9 +1,12 @@
 //! **Important.** The generated snapshots are specific to stable Rust; nightly Rust provides better spanning for types
 //! (`Option<u64>` instead of `Option`).
 
+use std::fmt;
+
 use anstream::AutoStream;
-use smart_config::{ConfigSchema, DescribeConfig};
+use smart_config::{ConfigSchema, DescribeConfig, Environment, ExampleConfig, SerializerOptions};
 use smart_config_commands::Printer;
+use test_casing::{test_casing, Product};
 
 use crate::configs::{create_mock_repo, TestConfig};
 
@@ -61,4 +64,62 @@ fn erroneous_config_debug() {
         .unwrap_err();
     let buffer = String::from_utf8(buffer).unwrap();
     insta::assert_snapshot!("debug_errors", buffer);
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Format {
+    Yaml,
+    Json,
+    Env,
+}
+
+impl fmt::Display for Format {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        formatter.write_str(match self {
+            Self::Yaml => "yaml",
+            Self::Json => "json",
+            Self::Env => "env",
+        })
+    }
+}
+
+impl Format {
+    const ALL: [Self; 3] = [Self::Yaml, Self::Json, Self::Env];
+}
+
+#[test_casing(6, Product((Format::ALL, [false, true])))]
+#[test]
+fn printing_config_example_yaml(format: Format, diff: bool) {
+    let example_config = TestConfig::example_config();
+    let options = if diff {
+        SerializerOptions::diff_with_default()
+    } else {
+        SerializerOptions::default()
+    };
+    let options = options.flat(matches!(format, Format::Env));
+    let example_json = options.serialize(&example_config);
+
+    let mut buffer = vec![];
+    match format {
+        Format::Yaml => {
+            Printer::custom(AutoStream::never(&mut buffer))
+                .print_yaml(&example_json.into())
+                .unwrap();
+        }
+        Format::Json => {
+            Printer::custom(AutoStream::never(&mut buffer))
+                .print_json(&example_json.into())
+                .unwrap();
+        }
+        Format::Env => {
+            let env = Environment::convert_flat_params(&example_json, "APP_");
+            Printer::custom(AutoStream::never(&mut buffer))
+                .print_yaml(&env.into())
+                .unwrap();
+        }
+    }
+
+    let buffer = String::from_utf8(buffer).unwrap();
+    let snapshot_name = format!("example_{}_{format}", if diff { "diff" } else { "full" });
+    insta::assert_snapshot!(snapshot_name, buffer);
 }
